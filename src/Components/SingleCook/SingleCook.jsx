32cad500cf63d9cf10f9719371ad976c0a4289ec
IshratMishu <ishratjahan0425@gmail.com>
2024-03-18 06:10:04 +0600

    currently cooking function done

--- src/Components/SingleCook/SingleCook.jsx
+++ src/Components/SingleCook/SingleCook.jsx
@@ -4,18 +4,15 @@ const SingleCook = ({singleCook, idx}) => {
     const { recipe_name, preparing_time, calories } = singleCook;
     return (
         <div>
-            <table className='font-firaSans font-medium text-[#878787]'>
-                <tbody className='mb-3'>
-                    <tr className='flex gap-5 space-y-4 items-center'>
+            <table className='font-firaSans font-medium text-[#878787] w-full mt-2'>
+                <tbody className='flex justify-between'>
                         <td className='text-black'>{idx + 1}</td>
                         <td className='w-1/4'>{recipe_name}</td>
                         <td className='w-1/4'>{preparing_time}</td>
-                        <td className='w-1/4'>{calories}</td>
-                    </tr>
+                        <td className='w-1/4'>{calories}</td>   
                 </tbody>
             </table>
-        </div>
-      
+        </div> 
     );
 };
 
